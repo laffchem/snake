d5fa788a344ef3aa061e7a2502669ac8d58bafd7
John Laffey <laffeychem@gmail.com>
2022-03-27 16:15:10 -0400

    Fixed variable rename

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ while game_on:
     #Detect Collision with Tail
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
+            game_on = False
             score.game_over()
 
 
